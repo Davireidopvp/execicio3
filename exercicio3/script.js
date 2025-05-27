99228db04a1d1072932ff5f2ce37c0cd510ba7b9
Davireidopvp <141867200+Davireidopvp@users.noreply.github.com>
2025-05-27 12:24:54 -0300

    Add files via upload

---
+++ exercicio3/script.js
@@ -0,0 +1,16 @@
+
+let numealuno
+
+
+numealuno = (window(""));
+
+if(parQuImpar == 0) {
+
+}
+if(resultado > 10) {
+    alert("           ");
+} else {
+    alert("           ");
+}
+
+alert(resultado);
